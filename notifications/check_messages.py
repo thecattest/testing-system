c109694fb529dc0fd81c85157b602171233bc364
ilya vodopyanov <vodopyanov999@gmail.com>
2020-05-03 17:17:05 +0000

    codes bot work

--- notifications/check_messages.py
+++ notifications/check_messages.py
@@ -26,7 +26,7 @@ def check_updates(db, User):
         bot = updater.bot
         try:
             with open('last.txt', 'r') as f:
-                update_id = int(f.read().strip())
+                update_id = int(f.read().strip()) + 1
         except FileNotFoundError:
             update_id = None
         updates = bot.get_updates(offset=update_id)
@@ -37,9 +37,11 @@ def check_updates(db, User):
 
 
 def check_update(db, logger, User, update):
-    code = update.message.text.strip()
+    code = update.message.text.strip().lower()
     user = db.query(User).filter(User.secret_code == code).first()
     chat_id = update.message.chat.id
+    with open('last.txt', 'w') as f:
+        f.write(str(update.update_id))
     if not user:
         logger.warning(f"Wrong code from {chat_id}")
         update.message.reply_text("Неверный код")
@@ -49,8 +51,6 @@ def check_update(db, logger, User, update):
             db.commit()
             update.message.reply_text(f"Аккаунт {user.nickname} привязан")
             logger.info(f"Аккаунт {user.nickname} привязан к {chat_id}")
-            with open('last.txt', 'w') as f:
-                f.write(str(update.update_id))
         except Exception as e:
             logger.error(f"{type(e)}: {str(e)}")
             update.message.reply_text(f"Произошла ошибка")
